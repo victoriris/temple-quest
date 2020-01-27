391a1b3e951b75cd1402bdc8b2aa82e4f9c763f9
Victor Iris <vmiris@harding.edu>
2020-01-27 09:03:16 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import Viewer from './Viewer/'; 
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <Viewer />
+      </div>
+    );
+  }
+}
+
+export default App;

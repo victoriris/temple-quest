b14c5ef908e743773145c20e7afd84b63945b0f9
Victor Iris <vmiris@harding.edu>
2020-01-31 14:57:33 -0600

    Refactor code for shake effect

--- src/screens/IntroScreen.js
+++ src/screens/IntroScreen.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Dimmer, Loader } from 'semantic-ui-react';
 import { stopIntroLoading } from '../actions';
 import TeamLogo from '../img/TeamLogo.svg';
 
@@ -8,25 +7,22 @@ import TeamLogo from '../img/TeamLogo.svg';
 class IntroScreen extends Component {
 
     componentDidMount () {
-    setTimeout(this.props.stopIntroLoading, 3000);
+        setTimeout(this.props.stopIntroLoading, 3000);
     }
     
     render() {
-        const imgShake = {
-            animation: "FadeIn 1.2s ease-in, Shake 0.1s ease-in 1.4s, FadeOut 1.2s ease-out 1.8s",
-            animationIterationCount: "1",
-        }
         
         return (
-            <div tabIndex="0" className="introScreen" onClick={this.props.stopIntroLoading} onKeyDown={this.props.stopIntroLoading} >
-                <div className="introScreen-logo" >
-                    <img src={TeamLogo} style={imgShake} class="fadeIn" alt=""/>
+            <div tabIndex="0" 
+            className="introScreen" 
+            onClick={this.props.stopIntroLoading} 
+            onKeyDown={this.props.stopIntroLoading} >
+                <div className="introScreen__logo" >
+                    <img src={TeamLogo} alt=""/>
                 </div>
-
-
-
             </div>
-        )
+        );
+
     }
 }
 

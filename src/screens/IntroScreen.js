9c679a4ed012c5096cab6285528031372e9b6e58
Victor Iris <victormidp@gmail.com>
2020-01-30 19:07:02 -0600

    Added glitch, fade in, and fade out animations. files: IntroScreen.js, _introScreen.scss (#1)
    Co-authored-by: Connor Albers <calbers1@harding.edu>

--- src/screens/IntroScreen.js
+++ src/screens/IntroScreen.js
@@ -8,18 +8,22 @@ import TeamLogo from '../img/TeamLogo.svg';
 class IntroScreen extends Component {
 
     componentDidMount () {
-        setTimeout(this.props.stopIntroLoading, 5000);
+     setTimeout(this.props.stopIntroLoading, 5000);
     }
 
     render() {
+        const imgShake = {
+            animation: "FadeIn 1.5s ease-in, Shake 0.199s ease-in 2s, FadeOut 1.5s ease-out 3.5s",
+            animationIterationCount: "1",
+        }
         return (
             <div className="introScreen">
                 <div className="introScreen-logo">
-                    <img src={TeamLogo} />
+                    <img src={TeamLogo} style={imgShake} class="fadeIn" alt=""/>
                 </div>
-                <Dimmer active>
+
                     <Loader />
-                </Dimmer>
+
             </div>
         )
     }

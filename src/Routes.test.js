ca9e501a9f93413f6ab373e55b435af58edf73c8
Victor Iris <vmiris@harding.edu>
2020-01-31 08:20:50 -0600

    Reorganize tests

--- src/Routes.test.js
+++ src/Routes.test.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import App from '../Routes';
+import App from './Routes';
 import { shallow } from 'enzyme';
 
 afterEach(cleanup);

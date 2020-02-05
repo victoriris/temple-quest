9837a66f7141c4bcbe5c4e014f581cf8d7505350
danderson376 <danderson3@harding.edu>
2020-02-05 15:59:55 -0600

    Add Diagonal handler for win check

--- src/actions/BoardActions.js
+++ src/actions/BoardActions.js
@@ -9,15 +9,25 @@ export const checkBoardWin = (pieceId) => {
         const piece = pieces.find(({ id }) => id === parseInt(pieceId));
         if (!piece) return;
 
+        // Check if a piece is a valid diagonal compared to a root piece
+        const isDiagonal = (rootPiece, comparedPiece) => {
+            const colDiff = (comparedPiece.location.column - rootPiece.location.column);
+            const rowDif = (comparedPiece.location.row - rootPiece.location.row);
+            const result = colDiff / rowDif;
+            return result === 1 || result === -1;
+        };
+
         // Get located pieces with same row OR column
         const neighbors = pieces.filter((p) => {
             if (!p.location) return false;
             return (
                 p.location.row === piece.location.row 
                 || p.location.column === piece.location.column
+                || isDiagonal(piece, p)
             );
         });
-        
+
+
         // Accumulate similarities per direction
         const directions = neighbors.reduce((acc, item) => {
 
@@ -32,7 +42,10 @@ export const checkBoardWin = (pieceId) => {
                 if (item.location.column === piece.location.column) {
                     acc['column'][key] = (acc['column'][key] || 0) + matches;
                 }
-
+                if(isDiagonal(piece, item)) {
+                    acc['diagonal'][key] = (acc['diagonal'][key] || 0) + matches;
+                }
+                
             });
 
             return acc;
@@ -40,7 +53,7 @@ export const checkBoardWin = (pieceId) => {
 
         // Check if some value in some direction totally matched
         const hasWon = Object.values(directions).some(direction => {
-            return Object.values(direction).some(value => value === 4);
+            return Object.values(direction).some(value => value === 4);   
         });
 
         const message = 'Game over. The winner is Player ' + (!isUserTurn ? 1 : 2);

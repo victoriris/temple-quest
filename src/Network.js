c8b3f95f4afcb9cae2d19af086fca2b30722f979
Victor Iris <vmiris@harding.edu>
2020-02-12 21:03:58 -0600

    Update connection setup

--- src/Network.js
+++ src/Network.js
@@ -1,7 +1,21 @@
 import Peer from 'peerjs';
 
+//yarn upgrade caniuse-lite browserslist
+export const peer = new Peer('victormidp', {
+    host: 'temple-quest-peerjs.herokuapp.com',
+    port: 80,
+    debug: 2,
+});
 
-export const peer = new Peer();
 peer.on('open', function(id) {
     console.log('My peer ID is: ' + id);
+    peer.listAllPeers(list => {
+        console.log(list)
+    });
+});
+
+peer.on('error', function({type}) {
+    if (type === 'unavailable-id') {
+        console.log('Id is taken already');
+    }
 });

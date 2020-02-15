ff81e6255d0e967138baf05f5757fd2b7b79ca3f
csmith53 <45865732+csmith53@users.noreply.github.com>
2020-02-15 11:43:39 -0600

    Add initial online game setup

--- src/screens/OnlineScreen.js
+++ src/screens/OnlineScreen.js
@@ -1,12 +1,68 @@
 import React, { Component } from 'react';
+import Peer from 'peerjs';
+import { initPeer } from '../actions/NetworkActions';
 
 class OnlineScreen extends Component{
+    constructor(props) {
+        super(props);
+        this.createPeer = this.createPeer.bind(this);
+        this.connectToPeer = this.connectToPeer.bind(this);
+        this.getPeersList = this.getPeersList.bind(this);
+        this.checkUsername = this.checkUsername.bind(this);
+        this.displayPeersList = this.displayPeersList.bind(this);
+    }
     render() {
         return(
             <div className="OnlineScreen">
-                {/*This screen is currently blank due to the necessity to have further discussion about how
-                this screen is going to be properly formatted.*/}
+                <div id="getUsernameDiv">
+                    <label for="getName">Username:</label>
+                    <input id="getName" type="text" pattern="[a-zA-Z]+" minLength="4" maxLength="10" placeholder="username" 
+                        title="Username should only contain letters, between 4 and 10 characters long"></input>
+                    <button id="userNameButton" onClick="checkUsername()">Submit</button>
+                </div>
+                <div id="peersList" display="none">
+                </div>
             </div>
         );
+    }
+
+    createPeer(peerId) {
+        try {
+            var peer = new Peer({key: peerId});
+            this.props.peer = peer;
+        }
+        catch (e) {
+            return false;
+        }
+    }
+    
+    getPeersList() {
+        var peersList = this.props.peer.listAllPeers(function cb(list){});
+        this.props.peersList = peersList;
+       
+    }
+
+    checkUsername() {
+       var userId = document.getElementById("getName").value;
+       if(this.createPeer(userId)) {
+         document.getElementById("getUsernameDiv").style.display = "none";
+         document.getElementById("peersList").style.display = "block";
+       }
+       else {
+           alert("This username is already taken, please choose a different one");
+       }
+    }
+
+    displaysPeersList() {
+        var html = '';
+        if(this.props.peersList.array.size() > 0){
+            this.props.peersList.array.forEach(element => {
+                html += '<button id=\'' + element.id + '\' onClick=\'connectToPeer(' + element.id + ')\'>' + element.id + '</button>';
+            });
+        }
+        else {
+            html = '<p>There are no other users to connect to and play with</p>';
+        }
+        document.getElementById("peersList").innerHTML = html;
     }
 }

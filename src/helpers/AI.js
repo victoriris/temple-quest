c6f4c90cd5e0f5259ed4c9290c758d8aa715b6f2
Victor Iris <vmiris@harding.edu>
2020-02-29 11:16:11 -0600

    Init make move

--- src/helpers/AI.js
+++ src/helpers/AI.js
@@ -174,13 +174,41 @@ const move = {
 };
 
 
+/* 
+Your generateMoves function must take a gameState object and
+ return an array of all legal moves for that game state. 
+You can represent the moves however 
+you see fit: integers, objects, strings, etc.
+ */
 function generateMoves(gameState) {
-    /* 
-    Your generateMoves function must take a gameState object and
-     return an array of all legal moves for that game state. 
-    You can represent the moves however 
-    you see fit: integers, objects, strings, etc.
-     */
+    const { pieces } = gameState;
+    let possibleMoves = [];
+
+    for (let row = 0; row < 16; row++) {
+        for (let column = 0; column < 16; column++) {
+            const isUsed = pieces.some((piece) => {
+                return DeepEqual(piece.location, {row, column});
+            });
+            if (!isUsed) {
+                for (let piece of pieces) {
+                    const move = {
+                        pieceId: piece.id, 
+                        location: { row, column }
+                    }
+                    possibleMoves.push(move);
+                }
+            }
+        }
+    }
+
+    return possibleMoves;
+}
+
+/* 
+Your makeMove function must take a gameState object and a move object, perform the move upon the gameState, altering it in place, and return a boolean value that represents whether or not the side-to-move has changed after having performed the move.
+ */
+function makeMove(gameState, move) {
+    
 }
 
 

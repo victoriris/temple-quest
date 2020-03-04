28dc041d874f35078e95dcbc4e1e5d1bc71ccb3d
Victor Iris <vmiris@harding.edu>
2020-03-03 21:44:05 -0600

    Cleanup code

--- src/helpers/AI.js
+++ src/helpers/AI.js
@@ -3,59 +3,18 @@ import DeepEqual from 'deep-equal';
 import negamaxAlphaBeta from 'negamax-alpha-beta';
 
 
-// var AlphaBetaResult ={
-//    coordinates: null, //x,y of selectedPiece
-//    piece: {bestPiece: null, score: -10000} //newPiece
-// }
-
-// let max;
-
-
-
-
 async function startMinimax(pieces, selectedPieceId) {
 
     return new Promise((resolve, reject) => {
         var gameState =  {
             pieces: [],
-            unusedPieces: [],
-            unusedLocations: [],
-            isUserTurn: false,
-            isGameOver: false,
-            selectedPieceId: '',
+            selectedPieceId,
             lastPieceID: '',
         }
 
-        let spaces = [];
-        for (let row = 0; row < 4; row++){
-            for (let column = 0; column < 4; column++){
-                spaces.push({row, column});
-            }
+        for (const piece of pieces) {
+            gameState.pieces.push({...piece})
         }
-
-       //create array of empty spaces, only taking an item if the return is false(and the space isn't filled)
-       const emptySpaces = spaces.filter((item)=>{
-           //switches the nested return
-           return !pieces.some((piece)=>{
-               //returns true if a piece has a location
-               return DeepEqual(piece.location, item);
-           })
-       })
-
-        let unused = pieces.filter((item)=>{
-            return !item.location;
-        })
-        let selectedPiece = unused.find(u => u.id === parseInt(selectedPieceId));
-        unused = unused.filter((u) => {
-        return !DeepEqual(u, selectedPiece);
-    });
-
-
-
-        gameState.pieces =  pieces.slice(0);
-        gameState.selectedPieceId = selectedPieceId;
-        gameState.unusedLocations = emptySpaces;
-        gameState.unusedPieces = unused;
 
         let config = {
             generateMoves,
@@ -73,141 +32,10 @@ async function startMinimax(pieces, selectedPieceId) {
             location: result.bestMove.location,
             pieceId: result.bestMove.pieceId
         });
-    //     //fill up spaces array
-    //     AlphaBetaResult.piece.score = -10000;
-    //     let spaces = [];
-    //     for (let row = 0; row < 4; row++){
-    //         for (let column = 0; column < 4; column++){
-    //             spaces.push({row, column});
-    //         }
-    //     }
-
-    //    //create array of empty spaces, only taking an item if the return is false(and the space isn't filled)
-    //    const emptySpaces = spaces.filter((item)=>{
-    //        //switches the nested return
-    //        return !pieces.some((piece)=>{
-    //            //returns true if a piece has a location
-    //            return DeepEqual(piece.location, item);
-    //        })
-    //    })
     
-    //       //find pieces without locations
-    //     const unused = pieces.filter((item)=>{
-    //         return !item.location;
-    //     })
-    //     //console.log("unused: ",unused);
-    
-    //     let selectedPiece = unused.find(u => u.id === parseInt(selectedPieceId));
-    //    // console.log("selectedPiece: ", selectedPiece);
-    //    let unusedCopy = unused.filter((u) => {
-    //     return !DeepEqual(u, selectedPiece);
-    // });
-    
-    //     const maxDepth = getDepth(unused.length);
-    //     console.log("Depth: ", maxDepth);
-    //     const bestMove = getBestMove(pieces, emptySpaces, selectedPiece, unusedCopy, maxDepth );
-    //     console.log("BESTMOVE: ", bestMove);
-
-        // Coordinates and piece
         
     });
 }
-
-//returns optimal location, optimal piece to give opponent
-// function getBestMove(board, emptyLocations, selectedPiece, unusedPieces, maxDepth){
-//     let result;
-//     let alpha, beta;
-//     console.log("Locations: ", emptyLocations);
-//     emptyLocations.forEach((location)=>{
-//     max = -10000;
-//     alpha = -Infinity;
-//     beta = Infinity;
-//     selectedPiece.location = location;
-//     let locationsCopy = emptyLocations.filter((l) =>{
-//         return !DeepEqual(l, selectedPiece.location);
-//     });
-//     let boardPiece = board.find(({ id }) => id === parseInt(selectedPiece.id));
-//     if (boardPiece){ 
-//     boardPiece.location = location;
-//     }
-//     result = getBoardScore(board, locationsCopy, selectedPiece, unusedPieces, 1, maxDepth, true, alpha, beta);
-//     // console.log("FINISHED ONE LOOP, RESULT: ", result, "LOCATION: ", location);
-//     console.log("result: ", result);
-//     if (result.score >= AlphaBetaResult.piece.score){
-//     AlphaBetaResult.coordinates = location;
-//     AlphaBetaResult.piece = result;
-//     console.log("current best: ", AlphaBetaResult);
-//     }
-//     selectedPiece.location = null;
-//     });
-
-
-//     console.log("final best: ",AlphaBetaResult)
-//     return AlphaBetaResult;
-
-
-// }
-
-
-// function getBoardScore( board, emptyLocations, selectedPiece, unusedPieces, currentDepth, maxDepth, flip, alpha, beta){
-//     //console.log("Current depth: ", currentDepth, "/", maxDepth);
-    
-//     //base cases
-//     //if someone wins:
-//     if (CheckWin(board, selectedPiece.id)){
-//         const bestPiece = {piece:selectedPiece, score:50}
-//         return bestPiece;
-//     }//if depth is reached (or tie)
-//     else if (currentDepth === maxDepth || emptyLocations.length === 0){
-//         const bestPiece = {piece:selectedPiece, score:0}
-//         return bestPiece;
-//     }
-    
-//     let boardCopy = [...board];
-//     let locationsCopy = emptyLocations.filter((l) =>{
-//         return !DeepEqual(l, selectedPiece.location);
-//     });
-//     let unusedCopy = unusedPieces.filter((u) => {
-//         return !DeepEqual(u, selectedPiece);
-//     });
-//     let currentPiece;
-//     var bestPiece = {piece: null, score: 0};
-//     locationsCopy.forEach((location,locationIndex)=>{
-//         unusedCopy.forEach((piece,unusedIndex)=>{
-//             currentPiece = piece;
-//             currentPiece.location = location;
-//            let boardPiece = boardCopy.find(({ id }) => id === parseInt(piece.id));
-//            if (boardPiece){ 
-//            boardPiece.location = location;
-//            }else {
-//                console.log("No Board Piece: " , boardPiece);
-//            }
-//             currentDepth++;
-//             let miniMaxResult = getBoardScore(boardCopy, locationsCopy, currentPiece, unusedCopy, currentDepth, maxDepth, !flip, -alpha, -beta); //MUST FLIP THE RESULT
-            
-//             if (flip){
-//             miniMaxResult.score = miniMaxResult.score * -1; //I DID IT I FLIPPED THE RESULT
-//             }
-//             currentPiece.location = null;
-//             boardPiece.location = null;
-//             if (miniMaxResult.score > max){
-//                 max = miniMaxResult.score;
-//                 //console.log("Max: ", max);
-//             }
-//             if (currentDepth === 2 && max === 50){
-//                 return {piece: currentPiece, score: Infinity}
-//             }
-//             currentDepth--;
-//                 bestPiece.piece = currentPiece;
-//                 bestPiece.score += miniMaxResult.score
-            
-//         })
-//     } )
-//     //console.log("Best Piece:", bestPiece);
-//     //bestPiece.score = max;
-//     return bestPiece;
-
-// }
 
 
 function getDepth(unusedPieces){
@@ -227,19 +55,8 @@ You can represent the moves however
 you see fit: integers, objects, strings, etc.
  */
 function generateMoves(gameState) {
-    const { pieces, unusedLocations, unusedPieces } = gameState;
+    const { pieces } = gameState;
     let possibleMoves = [];
-
-    // unusedLocations.forEach((location)=>{
-    //     unusedPieces.forEach((piece)=>{
-    //         const move = {
-    //             pieceId: piece.id,
-    //             location
-    //         }
-    //         possibleMoves.push(move);
-    //     });
-    // });
-
 
     for (let row = 0; row < 4; row++) {
         for (let column = 0; column < 4; column++) {
@@ -259,15 +76,14 @@ function generateMoves(gameState) {
         }
     }
 
-    //console.log('list returned')
     return possibleMoves;
 }
 
 /* 
 Your makeMove function must take a gameState object and a move object, perform the move upon the gameState, altering it in place, and return a boolean value that represents whether or not the side-to-move has changed after having performed the move.
  */
-function makeMove(gameState, move, flip = false) {
-    const {pieceId, location, selectedPieceId} = move;
+function makeMove(gameState, move) {
+    const { pieceId, location, selectedPieceId } = move;
 
     const updatedPiece = gameState.pieces.find((piece) => {
         if (piece.id === parseInt(selectedPieceId)) {
@@ -282,11 +98,6 @@ function makeMove(gameState, move, flip = false) {
         gameState.selectedPieceId = pieceId;
     }
 
-    // if (updatedPiece && !flip) {
-    //     gameState.lastPieceID = selectedPieceId;
-    //     gameState.selectedPieceId = pieceId;
-    // }
-
     return !!updatedPiece;
 }
 
@@ -294,7 +105,7 @@ function makeMove(gameState, move, flip = false) {
 Your unmakeMove function must take a gameState object and a move object, un-perform the move upon the gameState, altering it in place. This must end up producing the exact same gameState as before having called makeMove.
  */
 function unmakeMove (gameState, move) {
-    const {pieceId, location, selectedPieceId} = move;
+    const { selectedPieceId } = move;
 
     const updatedPiece = gameState.pieces.find((piece) => {
         if (piece.id === parseInt(gameState.lastPieceID)) {
@@ -305,20 +116,19 @@ function unmakeMove (gameState, move) {
     });
 
     if (updatedPiece) {
+        gameState.lastPieceID = '';
         gameState.selectedPieceId = selectedPieceId;
     }
 }
 
+/*
+If the current player wins, add 50 points. If current player doesn't win, they get 0. TODO: add threeInARow function
+*/
 function evaluate(gameState){
-    /*
-    If the current player wins, add 50 points. If current player doesn't win, they get 0. TODO: add threeInARow function
-    */
     if (CheckWin(gameState.pieces, gameState.lastPieceID)){
         return 50;
     }
     return threeInARow(gameState.pieces) * 3;
-    
-
 }
 
 function threeInARow(pieces){

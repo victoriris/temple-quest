1559e4cda822121954de7215ab6224d1c2da73e9
danderson376 <danderson3@harding.edu>
2020-03-06 17:47:13 -0600

    Map settings screen to muteSound and muteMusic

--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 import { Checkbox, Container, Grid, GridColumn, GridRow, Segment } from 'semantic-ui-react';
 import BackButton from '../components/BackButton';
+import { connect } from 'react-redux';
 import history from '../history';
+import { muteMusic, muteSound } from '../actions';
 
 
 class SettingsScreen extends Component{
     handleClick = (route) => history.push(route);
+
+    handleMuteMusicClick(){
+        this.props.muteMusic();
+    }
+
+    handleMuteSoundClick(){
+        this.props.muteSound();
+    }
+
     render() {
         return(
             <Grid stretched className="screen" padded centered columns={3}>
@@ -14,10 +25,10 @@ class SettingsScreen extends Component{
                     <Container className="screen__box">
                         <Segment.Group class="settingsGroup">
                             <Segment compact>
-                                <Checkbox toggle label="Mute Music" />
+                                <Checkbox toggle label="Mute Music" onChange={() => this.handleMuteMusicClick()}/>
                             </Segment>
                             <Segment compact>
-                                <Checkbox toggle label="Mute Sound" />
+                                <Checkbox toggle label="Mute Sound" onChange={() => this.handleMuteSoundClick()}/>
                             </Segment>
                         </Segment.Group>
                     </Container>
@@ -30,4 +41,13 @@ class SettingsScreen extends Component{
     }
 }
 
-export default SettingsScreen;
+const mapStateToProps = ({ audio }) => {
+    const { musicOn, soundOn} = audio;
+    return { musicOn, soundOn};
+};
+
+export default connect(mapStateToProps, { 
+    muteSound, muteMusic
+})(SettingsScreen);
+
+//export default SettingsScreen;
